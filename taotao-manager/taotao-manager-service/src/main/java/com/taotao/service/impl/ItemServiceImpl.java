26a9815ac729130dce0ffddea13670329784305b
hanshungithub <hshanshun@163.com>
2018-01-08 22:22:30 +0800

    commit

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -3,6 +3,8 @@ package com.taotao.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemExample;
@@ -10,6 +12,7 @@ import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -33,5 +36,18 @@ public class ItemServiceImpl implements ItemService {
         PageInfo<TbItem> pageInfo = new PageInfo<>(results);
         euDataGridResult.setTotal(pageInfo.getTotal());
         return euDataGridResult;
+    }
+
+    @Override
+    public TaotaoResult createItem(TbItem tbItem) {
+        long itemId = IDUtils.genItemId();
+
+        tbItem.setId(itemId);
+        tbItem.setStatus((byte) 1);
+        tbItem.setCreated(new Date());
+        tbItem.setUpdated(new Date());
+
+        itemMapper.insert(tbItem);
+        return TaotaoResult.ok();
     }
 }
